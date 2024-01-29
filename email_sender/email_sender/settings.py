20d6af1a432df60dec5a8216f474e865d7269412
anjalika27 <112751110+anjalika27@users.noreply.github.com>
2024-01-29 18:09:30 +0530

    Update settings.py

--- email_sender/email_sender/settings.py
+++ email_sender/email_sender/settings.py
@@ -131,7 +131,6 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'anjalika.agarwal@gmail.com'
-EMAIL_HOST_PASSWORD = "ubun kjky ryln fqgy"
+EMAIL_HOST_USER = 'your@gmail.com'
+EMAIL_HOST_PASSWORD = "your_password"
 
-# password="ubun kjky ryln fqgy"
